6a8fe6be0c50d5efb75566acd6c9a193df6f067f
Sam Carroll <cfcarroll321@gmail.com>
2019-01-17 22:24:23 -0800

    Working on main portion of Lab1.

---
+++ Lab1/ext2.h
@@ -0,0 +1,127 @@
+#ifndef EXT2_H
+#define EXT2_H
+
+typedef unsigned char u8;
+typedef unsigned short u16;
+typedef unsigned long u32;
+
+#define TRK 18
+#define CYL 36
+#define BLK 1024
+
+typedef struct ext2_group_desc  GD;
+typedef struct ext2_inode       INODE;
+typedef struct ext2_dir_entry_2 DIR;
+
+GD    *gp;
+INODE *ip;
+DIR   *dp;
+
+char buf1[BLK], buf2[BLK];
+u16 color = 0x0A;
+u8 ino;
+u16 NSEC = 2;
+
+struct ext2_inode {
+	u16	i_mode;		/* File mode */
+	u16	i_uid;		/* Owner Uid */
+	u32	i_size;		/* Size in bytes */
+	u32	i_atime;	/* Access time */
+	u32	i_ctime;	/* Creation time */
+	u32	i_mtime;	/* Modification time */
+	u32	i_dtime;	/* Deletion Time */
+	u16	i_gid;		/* Group Id */
+	u16	i_links_count;	/* Links count */
+	u32	i_blocks;	/* Blocks count */
+	u32	i_flags;	/* File flags */
+    
+	u32     dummy;
+	u32	i_block[15];    /* Pointers to blocks */
+    u32     pad[7];         /* inode size MUST be 128 bytes */
+};
+
+struct ext2_group_desc
+{
+	u32	bg_block_bitmap;		/* Blocks bitmap block */
+	u32	bg_inode_bitmap;		/* Inodes bitmap block */
+	u32	bg_inode_table;		/* Inodes table block */
+	u16	bg_free_blocks_count;	/* Free blocks count */
+	u16	bg_free_inodes_count;	/* Free inodes count */
+	u16	bg_used_dirs_count;	/* Directories count */
+	u16	bg_pad;
+	u32	bg_reserved[3];
+};
+
+struct ext2_dir_entry_2 {
+	u32	inode;			/* Inode number */
+	u16	rec_len;		/* Directory entry length */
+	u8	name_len;		/* Name length */
+	u8	file_type;
+	char	name[255];      	/* File name */
+};
+
+u16 prints(char *s)
+{
+    while(*s)
+    {
+        putc(*s++);
+    }
+}
+
+
+u16 gets(char *s) 
+{
+    while((*s = getc()) != '\r')
+    {
+        putc(*s++);
+    }
+    *s = '\0';
+}
+
+
+u16 getblk(u16 blk, char *buf)
+{
+    readfd( (2*blk)/CYL, ( (2*blk)%CYL)/TRK, ((2*blk)%CYL)%TRK, buf);
+}
+
+// Will modify code from Lab1.2 to find given name
+u16 search(INODE *pip, char *name)
+{
+  u16    i, iblk, count, size;
+  char   c, temp[64];
+
+// Step through the iblocks 0-11 (the direct iblocks)
+   for(i = 0; i < 12; i++)
+   {
+       if(pip->i_block[i] == 0)
+       {
+           break;
+       }
+	   size = BLK;
+// Get block for each block in given inode
+       getblk((u16)pip->i_block[i], buf2);
+       dp = (DIR*)buf2;
+// Search block for name match
+       while(count < size && dp->inode)
+       {
+           memcpy(temp, dp->name, dp->name_len);
+           temp[dp->name_len] = '\0';
+// If found return inode number
+		   if(!strcmp(temp, name))
+		   {
+			  prints(temp); putc('\n'); putc('\r');
+			  return (u16)(dp->inode - 1);
+		   }
+		   prints(temp);
+		   putc(' ');
+           count += dp->rec_len;
+           dp = (DIR*)((char*)dp + dp->rec_len);
+       }
+   }
+// Return error if not found
+   prints("Error: name could not be found\n\r");
+   error();
+   return -1;
+}
+
+#endif
---
+++ Lab1/t.c
@@ -0,0 +1,78 @@
+#include "ext2.h"
+
+main()
+{
+  u16 i, iblk, offset, blk;
+  u32 *up;
+  char c, temp[64];
+
+  char file1[10], file2[10];
+
+  strcpy(file1, "boot");
+  strcpy(file2, "mtx");
+  // Read block 2 to get the inode starting block
+  getblk(2, buf1);
+
+  // 1. WRITE YOUR CODE to get iblk = bg_inode_table block number
+
+  gp = (GD *)buf1;
+  iblk = gp->bg_inode_table;
+
+  // 2. WRITE YOUR CODE to get root inode
+
+  // Offet by 1 to get root
+  getblk(iblk, buf1);
+  ip = (INODE *)buf1 + 1;
+
+  // Find /boot in root
+  i = search(ip, file1);
+  getc();
+  blk = (i / 8) + iblk;
+  offset = i % 8;
+  getblk(blk, buf1);
+  ip = (INODE *)buf1 + offset;
+
+  i = search(ip, file2);
+  getc();
+  // blk = (i / 8) + iblk;
+  // offset = i % 8;
+  // getblk(blk, buf1);
+  // ip = (INODE *)buf1 + offset;
+
+  // // Write YOUR C code to get the INODE of /boot/mtx
+  // // INODE *ip --> INODE
+  // blk = 0;
+  // //  if INODE has indirect blocks: get i_block[12] int buf2[  ]
+  // if (ip->i_block[12] != 0)
+  // {
+  //   getblk((u16)ip->i_block[12], buf2);
+  //   blk = 1;
+  // }
+
+  // setes(0x1000); // MTX loading segment = 0x1000
+
+  // // load 12 DIRECT blocks of INODE into memory
+  // for (i = 0; i < 12; i++)
+  // {
+  //   getblk((u16)ip->i_block[i], 0);
+  //   putc('*');
+  //   inces();
+  //   getc();
+  // }
+
+  // if (blk)
+  // {
+  //   up = (u32 *)buf2;
+  //   // load INDIRECT blocks, if any, into memory
+  //   while (*up)
+  //   {
+  //     getblk((u16)*up, 0);
+  //     putc('.');
+  //     inces();
+  //     up++;
+  //   }
+  // }
+
+  prints("go?");
+  getc();
+}
