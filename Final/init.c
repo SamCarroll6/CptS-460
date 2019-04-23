081110617d61012e0077c0b4e975991e692b6ccd
Sam Carroll <cfcarroll321@gmail.com>
2019-04-23 16:00:48 -0700

    Init seems to be working.

--- Final/init.c
+++ Final/init.c
@@ -1,6 +1,6 @@
 #include "ucode.c"
 
-int console;
+int console, console2, console3;
 
 int parent()
 {
@@ -23,19 +23,67 @@ int parent()
             }
             printf("Sams INIT : I just buried an orphan child proc %d\n", pid);
         }
+        else if(pid == console2)
+        {
+            printf("Sams INIT : forks a new console login\n");
+            console2 = fork();
+            if(console2)
+            {
+                continue;
+            }
+            else
+            {
+                exec("login /dev/ttyS0");
+            }
+            printf("Sams INIT : I just buried an orphan child proc %d\n", pid);
+        }
+        if(pid == console3)
+        {
+            printf("Sams INIT : forks a new console login\n");
+            console3 = fork();
+            if(console3)
+            {
+                continue;
+            }
+            else
+            {
+                exec("login /dev/ttyS1");
+            }
+            printf("Sams INIT : I just buried an orphan child proc %d\n", pid);
+        }
     }
 }
 
 int main(int argc, char *argv[ ])
 {
-    int in, out;
+    int in, out, in2, out2, in3, out3;
     in = open("/dev/tty0", O_RDONLY);
     out = open("/dev/tty0", O_WRONLY);
+    in2 = open("/dev/ttyS0", O_RDONLY);
+    out2 = open("/dev/ttyS0", O_WRONLY);
+    in3 = open("/dev/ttyS1", O_RDONLY);
+    out3 = open("/dev/ttyS1", O_WRONLY);
     printf("Sams INIT : fork a login proc on console\n");
     console = fork();
     if(console)
     {
-        parent();
+        console2 = fork();
+        if(console2)
+        {
+            console3 = fork();
+            if(console3)
+            {
+                parent();
+            }
+            else
+            {
+                exec("login /dev/ttyS1");
+            }
+        }  
+        else
+        {
+            exec("login /dev/ttyS0");
+        }
     }
     else
     {
---
+++ Final/login.c
@@ -0,0 +1,11 @@
+#include "ucode.c"
+
+int in, out, err;
+char name[128], password[128];
+
+int main(int argc, char *argv[ ])
+{
+    close(0);
+    close(1);
+    open
+}
