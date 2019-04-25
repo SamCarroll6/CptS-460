444814009715b58a4a8943a5adaaeee748ecf80f
Sam Carroll <cfcarroll321@gmail.com>
2019-04-24 17:00:27 -0700

    Grep works, just adding a header in more file.

--- Final/more.c
+++ Final/more.c
@@ -2,5 +2,7 @@
 
 int main(int argc, char *argv[ ])
 {
-    
+    prints("****************************\n");
+    prints("          Sams More         \n");
+    prints("****************************\n");
 }
